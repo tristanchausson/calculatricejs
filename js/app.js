38abd901498f73bd24edd2aabdcec3cfc7247c76
Elisa Demanez <demanez.elisa@gmail.com>
2017-11-23 14:55:06 +0100

    fonction 7 qui fonctionne

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 var touches = document.querySelectorAll('div.touches');
 var operateurs = ['+',"-","/","x"]
+
+function fsept() 
+
+{
+	var x=document.getElementById("isept").value;
+		console.log(x);
+}
+
+
