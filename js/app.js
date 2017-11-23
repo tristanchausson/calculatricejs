b546f5f456fc94b31e9d86df9bba486897e0feef
Elisa Demanez <demanez.elisa@gmail.com>
2017-11-23 15:13:12 +0100

    fonction 7 et 8 s'affichent dans l'input

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 var touches = document.querySelectorAll('div.touches');
 var operateurs = ['+',"-","/","x"]
 
-function fsept() 
+function sept() 
 
 {
-	var x=document.getElementById("isept").value;
-		console.log(x);
+	var y=document.getElementById("sept").value;
+		console.log(y);
+	var valeur=document.getElementById("input").value;
+	document.getElementById('input').value=valeur+y;
+
 }
 
+function huit() 
+
+{
+	var y=document.getElementById("huit").value;
+		console.log(y);
+	var valeur=document.getElementById("input").value;
+	document.getElementById('input').value=valeur+y;
+
+}
 
