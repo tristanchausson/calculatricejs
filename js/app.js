d42475b597a48eaea8ba3cb8f44e64d2cff079b5
tristanchausson <tristanchausson@me.com>
2017-11-23 17:06:13 +0100

    ajout de tous les nombres

--- js/app.js
+++ js/app.js
@@ -1,24 +1,85 @@
 /* var touches = document.querySelectorAll('div.touches'); */
 var operateurs =""
 var total=""
-function sept() 
 
+function zéro() 
+{
+	var y=document.getElementById("zero").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function un() 
+{
+	var y=document.getElementById("un").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function deux() 
+{
+	var y=document.getElementById("deux").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function trois() 
+{
+	var y=document.getElementById("trois").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function quatre() 
+{
+	var y=document.getElementById("quatre").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function cinq() 
+{
+	var y=document.getElementById("cinq").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function six() 
+{
+	var y=document.getElementById("six").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
+}
+
+function sept() 
 {
 	var y=document.getElementById("sept").value;
 		console.log(y);
 	var valeur=document.getElementById("affichage").value;
 	document.getElementById('affichage').value=valeur+y;
-
 }
 
 function huit() 
-
 {
 	var y=document.getElementById("huit").value;
 		console.log(y);
 	var valeur=document.getElementById("affichage").value;
 	document.getElementById('affichage').value=valeur+y;
+}
 
+function neuf() 
+{
+	var y=document.getElementById("neuf").value;
+		console.log(y);
+	var valeur=document.getElementById("affichage").value;
+	document.getElementById('affichage').value=valeur+y;
 }
 
 function retour() 
